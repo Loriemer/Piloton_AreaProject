33ee43f0094a36436c8f8a439ea98e079149c314
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 01:54:29 +0800

    Add project files.

---
+++ Piloton_AreaProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PilotonAreaProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int lenght;
+            int width;
+
+            int area;
+
+            Console.WriteLine("Enter lenght: ");
+            lenght = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter width: ");
+            width = int.Parse(Console.ReadLine());
+
+            area = (lenght * width);
+
+            Console.WriteLine($"area: {area}");
+        }
+    }
+}
